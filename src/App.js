31a6a08e1d49d82d98e65d3e2a0c08f6daeeb85f
Trummm <trungpro2000.tp2@gmail.com>
2023-10-07 10:45:45 +0700

    first setupu

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+import { Box } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <Box width='400px'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  )
+}
+
+export default App
